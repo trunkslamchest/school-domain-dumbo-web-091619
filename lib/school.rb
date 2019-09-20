3683b4329c7cdec1f4c4506606a108bf634d6b30
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 15:26:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  
+  attr_accessor :roster
+  attr_reader :name
+  
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+  
+  def add_student(name, grade)
+    roster[grade] = [name]
+  end
+  
+end
+
+school = School.new("Test School")
+p school
+# school.add_student("AC Slater", 10)
